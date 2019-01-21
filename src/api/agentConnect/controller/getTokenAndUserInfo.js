2750e23991d980e75a64c5f260edbacacbbc2c0a
valentin bonnard <vbonnard@kaliopmail.com>
2019-01-21 15:39:09 +0100

    [ACBE] Add axios and remove async await fir promise

--- src/api/agentConnect/controller/getTokenAndUserInfo.js
+++ src/api/agentConnect/controller/getTokenAndUserInfo.js
@@ -1,7 +1,7 @@
+const axios = require('axios');
 const config = require('../../../config');
 
 const getTokenAndUserInfos = (req, res, next) => {
-    console.log('req', req.params);
     let response = {
         status: 'success',
         userInfos: {}
@@ -18,59 +18,80 @@ const getTokenAndUserInfos = (req, res, next) => {
         next();
     }
 
-    // Set request params.
-    const body = {
-        grant_type: 'authorization_code',
-        redirect_uri: `${config.FS_URL}${config.CALLBACK_FS_PATH}`,
-        client_id: config.CLIENT_ID,
-        client_secret: config.CLIENT_SECRET,
-        code: req.params.code,
-    };
 
-    try {
-        // Request access token.
-        const { data: { access_token: accessToken, id_token: idToken } } = await axios({
-            method: 'POST',
-            headers: { 'content-type': 'application/x-www-form-urlencoded' },
-            data: querystring.stringify(body),
-            url: `${config.FC_URL}${config.TOKEN_FC_PATH}`,
+    requestTokenWithCode(config, req.params.code)
+        .then((tokenRes) => {
+            return requestUserInfo(config, tokenRes.data.access_token, axios);
+        })
+        .then((infosRes) => {
+            console.log('user infos : ', infosRes.data);
+            res.render('userInfo', getRenderObj(infosRes));
+        })
+        .catch((err) => {
+            res.send(err.message);
         });
 
-        // Make a call to the France Connect API endpoint to get user data.
-        if (!accessToken) {
-            response = {
-                status: 'failed',
-                message: `No AccessToken`
-            }
-    
-            req.response = response;
-            next();
-        }
+};
 
-        response.accessToken = accessToken;
-        response.session.accessToken = accessToken;
-        response.session.idToken = idToken;
 
-        const { data: userInfo } = await axios({
-            method: 'GET',
-            headers: { Authorization: `Bearer ${accessToken}` },
-            url: `${config.FC_URL}${config.USERINFO_FC_PATH}`,
-        });
+/**
+* request on token url with customAxios to prevent self signed error
+* @param  {Object} params
+* @param  {String} code   [code retrieved with authorize]
+* @param  {Object} axios
+* @return {Promise}
+*/
 
-        // Helper to set userInfo value available to the profile page.
-        req.response.userInfos = userInfo;
-
-    } catch (error) {
-        response = {
-            status: 'failed',
-            message: `${error}`
-        }
-
-        req.response = response;
-        next();
+const requestTokenWithCode = (params, code, axios) => {
+    if (!code) {
+        return Promise.reject(new Error(`Error fetching your code
+        verify your openIdParameters : ${params}`));
     }
 
-    next();
+    return axios.post(`${params.FC_URL}${params.TOKEN_FC_PATH}`, {
+        redirect_uri: `${params.FS_URL}${params.CALLBACK_FS_PATH}`,
+        client_id: params.CLIENT_ID,
+        client_secret: params.CLIENT_SECRET,
+        grant_type: 'authorization_code',
+        code: code,
+    })
+        .catch((err) => {
+            return Promise.reject(new Error(getErrorFromAxiosRes(err)));
+        });
+};
+
+/**
+ * get userInfo with axios to prevent self signed error
+ * @param  {Object} params
+ * @param  {String} access_token
+ * @param  {Object} axios
+ * @return {Promise}
+ */
+
+const requestUserInfo = (params, accessToken, axios) => {
+    if (!accessToken) {
+        return Promise.reject(new Error(`Error fetching token.
+        Verify your openIdParameters.tokenUrl : ${params.FC_URL}${params.TOKEN_FC_PATH}`));
+    }
+
+    return axios.get(`${params.FC_URL}${params.USERINFO_FC_PATH}?schema=openid`,
+        { headers: { Authorization: `Bearer ${accessToken}` } })
+        .catch((err) => {
+            Promise.reject(new Error(getErrorFromAxiosRes(err)));
+        });
+};
+
+const getErrorFromAxiosRes = (res) => {
+    if (res.response.data.error) {
+        return `Error fetching user infos : ${res.response.data.error}, check your clientID and clientSecret`;
+    } else if (res.code && res.response && res.response.data.message) {
+        return `Error fetching user infos : err Code : ${res.code}, err message : ${res.response.data.message}`;
+    } else if (res.code) {
+        return `Error fetching user infos : err Code : ${res.code}`;
+    } else if (res.response && res.response.data.message) {
+        return `Error fetching user infos : err message : ${res.response.data.message}`;
+    }
+    return '';
 };
 
 module.exports = {
