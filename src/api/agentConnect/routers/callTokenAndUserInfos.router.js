9bc73265ed6f7b16af332ae7633c286b268560ef
valentin bonnard <vbonnard@kaliopmail.com>
2019-02-27 11:59:10 +0100

    [Fix back] remove * from route

--- src/api/agentConnect/routers/callTokenAndUserInfos.router.js
+++ src/api/agentConnect/routers/callTokenAndUserInfos.router.js
@@ -2,7 +2,7 @@ const controller = require('../controller/index');
 const middleware = require('../../../lib/express/customMiddleware/checkToken.middleware');
 
 module.exports = (app) => {
-    app.get('/api/agentConnect/callToken/:code?*', middleware.checkToken, controller.getTokenAndUserInfos, (req, res) => {
+    app.get('/api/agentConnect/callToken/:code', middleware.checkToken, controller.getTokenAndUserInfos, (req, res) => {
         if (res.information.status === 200) {
             res.send(res.information.data);
 
