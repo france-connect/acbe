2c7db4ae4761efd424a9309fdeccc5b330fb7916
valentin bonnard <vbonnard@kaliopmail.com>
2019-03-25 11:47:01 +0100

    [FIX] Add post verb

--- src/api/agentConnect/routers/logout.router.js
+++ src/api/agentConnect/routers/logout.router.js
@@ -2,5 +2,5 @@ const controller = require('../controller/index');
 const middleware = require('../../../lib/express/customMiddleware/checkToken.middleware');
 
 module.exports = (app) => {
-    app.get('/api/agentConnect/logout', middleware.checkToken, controller.setLogout);
+    app.post('/api/agentConnect/logout', middleware.checkToken, controller.setLogout);
 }
